8c472b23d0dd4cbce26a78220b0733eb456c539b
Floggony <mirrozz@gmail.com>
2026-06-03 11:05:26 +0700

    Update config.h

--- keyboards/bastardkb/charybdis/4x6/keymaps/vendor/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vendor/config.h
@@ -21,6 +21,9 @@
 #    define DYNAMIC_KEYMAP_LAYER_COUNT 4
 #endif // VIA_ENABLE
 
+#define VIA_EXPECTED_VERSION 0x000c // Форсирует протокол v12 (0x000c = 12)
+#define VIA_EXPECTED_VERSION 0x000c // Форсирует протокол v12 (0x000c = 12)
+
 #ifndef __arm__
 /* Disable unused features. */
 #    define NO_ACTION_ONESHOT
